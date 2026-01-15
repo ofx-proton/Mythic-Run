758907d82f5b68c9aae48b498a62a5ae5397ec6a
ofx-proton <251812551+ofx-proton@users.noreply.github.com>
2026-01-15 16:29:28 +0545

    Added Transition with bugs

--- scripts/start.gd
+++ scripts/start.gd
@@ -1,12 +1,23 @@
 extends Control
 
-func _ready():
-	pass
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("click_enter") :
-		get_tree().change_scene_to_file("res://scenes/game.tscn")
-	
+@onready var transition: CanvasLayer = $Transition
 func _on_start_pressed():
+	await transition.fade(1.0, 1.5).finished
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+	
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("click_enter"):
+		await start_game()
+
+
+	
+
+func start_game() -> void:
+	await transition.fade(1.0, 1.5).finished
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+	
+
 	
